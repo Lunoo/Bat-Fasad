94d36b909c2543855a27cfb47d2018853c3b616e
Zeratulis <churpita.roman@gmail.com>
2016-07-25 12:10:17 +0300

    12.10 - finish static html code

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,16 @@ $(function(){
         $top = $parent.next().offset().top;
         $('body,html').animate({scrollTop: $top}, 1000);
     })
+
+    $('.gallery a').attr('rel', 'gallery').fancybox({
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
 })
