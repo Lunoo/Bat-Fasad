7b996944a9840a725206a3e7db42feddd23ca14f
Zeratulis <churpita.roman@gmail.com>
2016-07-25 16:03:14 +0300

    16.05 - start animation

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,19 @@ $(function(){
 			}
 		}
 	});
+
+    setTimeout(function(){
+    	$('.price, .download, .section-1 .section-title').addClass('animate');
+    }, 1000);
+    var height = $(window).height();
+    $(window).scroll(function(){
+    	$this = $(this);
+        $('.advantage, .section-title, .reason, .section-4 .wrapper>div, .section-4 figure').each(function(i, item){
+        	// console.log($this.scrollTop() + height);
+        	// console.log($(item).offset().top)
+        	if( $this.scrollTop() + height >  $(item).offset().top + 100){
+        		$(item).addClass('animate');	
+        	}
+        });
+    })
 })
